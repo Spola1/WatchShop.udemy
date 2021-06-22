99ea4875b1f4c98e89896923f4f1ca6a16bddd58
Spola1 <1111.22224@mail.ru>
2021-06-22 11:57:45 +0300

    d1

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :product,  only: [:show]
+
+  resources :category, only: [:show]
+
+  root to: 'main#index'
 end
